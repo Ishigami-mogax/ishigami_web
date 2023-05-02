b4c89022df3bae222a3acf97bdb2466237538514
mogax <mogasrx@gmail.com>
2023-05-02 10:25:28 +0200

    Setup TsLint/workflow

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { RouterProvider } from "react-router-dom"
 import router from "./routes"
 import "./utils/i18n"
 
-const App = () => {
+const App:React.FC = (): JSX.Element => {
     return <div>
         <RouterProvider router={router} />
     </div>
