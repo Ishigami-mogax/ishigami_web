4ca5710b04ec0292ea22df08da4e44e41798dd56
Jarquen <80979553+Jarquen@users.noreply.github.com>
2023-05-09 11:52:31 +0200

    Add children

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { RouterProvider } from "react-router-dom"
 import router from "./routes"
 import "./utils/i18n"
+import {styles} from "./styles/App.style";
 
 const App:React.FC = (): JSX.Element => {
-    return <div style={{height: "100%"}}>
+    const {principalDiv} = styles
+
+    return <div style={principalDiv}>
         <RouterProvider router={router} />
     </div>
 }
--- src/components/_global/Navbar/Navbar.constant.ts
+++ src/components/_global/Navbar/Navbar.constant.ts
@@ -1,5 +1,3 @@
-import HomeIcon from '@mui/icons-material/Home';
-
 export interface PropsInterface {
 
 }
