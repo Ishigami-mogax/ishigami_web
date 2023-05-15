07710d423dc84c849d2be90849788e2ea8490a64
mogax <mogasrx@gmail.com>
2023-05-15 18:57:56 +0200

    Start add animation card

--- src/components/Session/Method/PairIt/PairIt.style.js
+++ src/components/Session/Method/PairIt/PairIt.style.js
@@ -1,3 +1,35 @@
 export const styles = {
-
+    mainContainer:{
+        margin:"auto",
+        width:'75%'
+    },
+    gridContainer:{
+        display:'grid'
+    },
+    topCard:{
+        margin:1,
+        gridRow:1,
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        height:140,
+        width:"80%",
+        transition:"background-color 0.25s, opacity 1s"
+    },
+    bottomCard:{
+        margin:1,
+        gridRow:2,
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        height:140,
+        width:"80%",
+        transition:"background-color 0.25s, opacity 1s"
+    },
+    itemTypo : {
+        fontWeight:'700',
+        fontSize:24,
+        wordBreak:'break-word',
+        textAlign:'center'
+    }
 }
--- src/components/Session/Method/PairIt/PairIt.tsx
+++ src/components/Session/Method/PairIt/PairIt.tsx
@@ -1,69 +1,23 @@
 import {FC, PropsWithChildren, useEffect, useState} from 'react'
-import {Box, Button, Card, CardContent, Typography} from "@mui/material"
+import {Box, Button, Card, CardContent, Fade, Typography} from "@mui/material"
 import {styles} from "./PairIt.style"
 import {PropsInterface} from "./PairIt.constant";
 import {useTranslation} from "react-i18next";
+import { useSpring, animated } from 'react-spring';
 
 const PairIt: FC = (props:PropsWithChildren<PropsInterface>) : JSX.Element => {
 
     //region Default
-    const {} = styles
+    const { gridContainer, mainContainer, topCard, bottomCard, itemTypo } = styles
     const {} = props
     const { t } = useTranslation()
-    let listTop = [
-        {
-            id:"R-1",
-            value:'Inu',
-            validate:false
-        },
-        {
-            id:"R-2",
-            value:'Neko',
-            validate:false
-        },
-        {
-            id:"R-3",
-            value:'Uma',
-            validate:false
-        },
-        {
-            id:"R-4",
-            value:'Ushi',
-            validate:false
-        },
-        {
-            id:"R-5",
-            value:'Dakuda',
-            validate:false
-        }
-    ]
-    let listBottom = [
-        {
-            id:"L-1",
-            value:'Cheval',
-            validate:false
-        },
-        {
-            id:"L-2",
-            value:'Dromadaire',
-            validate:false
-        },
-        {
-            id:"L-3",
-            value:'Chien',
-            validate:false
-        },
-        {
-            id:"L-4",
-            value:'Chat',
-            validate:false
-        },
-        {
-            id:"L-5",
-            value:'Vache',
-            validate:false
-        }
-    ]
+
+    const [shaking, setShaking] = useState(false)
+
+    const shakeAnimation = useSpring({
+        transform: shaking ? 'translate3d(0, 0, 0)' : 'translate3d(-10px, 0, 0)',
+        config: { tension: 300, friction: 10 },
+    })
 
     //endregion
 
@@ -74,26 +28,79 @@ const PairIt: FC = (props:PropsWithChildren<PropsInterface>) : JSX.Element => {
     //endregion
 
     //region UseState
-    const [right, setRight] = useState<any>(null)
-    const [left, setLeft] = useState<any>(null)
+    const [top, setTop] = useState([
+        {
+            id:"1",
+            value:'Inu',
+            validate:false
+        },
+        {
+            id:"2",
+            value:'Neko',
+            validate:false
+        },
+        {
+            id:"3",
+            value:'Uma',
+            validate:false
+        },
+        {
+            id:"4",
+            value:'Ushi',
+            validate:false
+        },
+        {
+            id:"5",
+            value:'Dakuda',
+            validate:false
+        }
+    ])
+    const [bottom, setBottom] = useState([
+        {
+            id:"1",
+            value:'Cheval',
+            validate:false
+        },
+        {
+            id:"2",
+            value:'Dromadaire',
+            validate:false
+        },
+        {
+            id:"3",
+            value:'Chien',
+            validate:false
+        },
+        {
+            id:"4",
+            value:'Chat',
+            validate:false
+        },
+        {
+            id:"5",
+            value:'Vache',
+            validate:false
+        }
+    ])
+
+    const [right, setRight] = useState<string>("")
+    const [left, setLeft] = useState<string>("")
     //endregion
 
     //region UseEffect
     useEffect(() => {
-        console.log(right)
-        console.log(left)
-
+        console.log("R:", right)
+        console.log("L:", left)
         if(right && left) {
-            if(right.id.slice(2) == left.id.slice(2)) {
-                console.log("pass")
-                listTop = listTop.map((e) => e.id === right.id ? { ...e, validate: true } : e)
-                listBottom = listBottom.map((e) => e.id === right.id ? { ...e, validate: true } : e)
+
+            if(right == left) {
+                top.find((e) => e.id === right)!!.validate = true
+                bottom.find((e) => e.id === left)!!.validate = true
             }
-            setRight(null)
-            setLeft(null)
+
+            setRight("")
+            setLeft("")
         }
-
-
     }, [right, left])
     //endregion
 
@@ -101,23 +108,32 @@ const PairIt: FC = (props:PropsWithChildren<PropsInterface>) : JSX.Element => {
     //endregion
 
     return (
-        <Box sx={{margin:"auto", width:'75%'}}>
+        <Box sx={mainContainer}>
             <Card><CardContent>
-                <Box sx={{display:'grid'}}>
-                    {listTop.map((w, index)=> (
-                        <Card sx={{margin:1, gridRow:1, gridColumn:index+1, display:'flex', justifyContent:'center', alignItems:'center', height:140, width:"80%"}}
-                              onClick={() => setRight(w)}><CardContent>
-                            <Typography sx={{fontWeight:'700', fontSize:24, wordBreak:'break-word', textAlign:'center'}}>{w.value}</Typography>
-                        </CardContent></Card>
+                <Box sx={gridContainer}>
+                    {top.map((w, index)=> (
+                        <Card sx={{...topCard, gridColumn:index+1,
+                            ...(w.id == right && {backgroundColor:'#D9D9D9'}),
+                            ...(w.validate && {backgroundColor:'#5DC852', opacity:0, })
+                        }}
+                              onClick={() => setRight(w.id)}>
+                            <CardContent>
+                                <Typography sx={itemTypo}>{w.value}</Typography>
+                            </CardContent>
+                        </Card>
                     ))}
-                    {listBottom.map((w, index)=> (
-                        <Card sx={{margin:1, gridRow:2, gridColumn:index+1, display:'flex', justifyContent:'center', alignItems:'center', height:140, width:"80%"}}
-                              onClick={() => setLeft(w)}><CardContent>
-                            <Typography sx={{fontWeight:'700', fontSize:24, wordBreak:'break-word', textAlign:'center'}}>{w.value}</Typography>
-                        </CardContent></Card>
+                    {bottom.map((w, index)=> (
+                        <Card sx={{...bottomCard, gridColumn:index+1,
+                            ...(w.id == left && {backgroundColor:'#D9D9D9'}),
+                            ...(w.validate && {backgroundColor:'#5DC852', opacity:0, })
+                        }}
+                              onClick={() => setLeft(w.id)}>
+                            <CardContent>
+                                <Typography sx={itemTypo}>{w.value}</Typography>
+                            </CardContent>
+                        </Card>
                     ))}
                 </Box>
-                <Button onClick={() => console.log(listTop)}>Test</Button>
             </CardContent></Card>
         </Box>
     );
